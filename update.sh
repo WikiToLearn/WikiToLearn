6b138e54cbe7d6a87dc81396a9f7aadb58931173
Riccardo Iaconelli <riccardo@kde.org>
2015-05-22 13:19:10 +0200

    update
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- update.sh
+++ update.sh
@@ -7,12 +7,15 @@ git pull
 # Update all the submodules
 git submodule foreach 'git checkout master; git pull || :'
 
-# Pull in newest content (?)
+# Pull in newest content (?) XML
 
-# rm -r mediawiki/extensions
-# ln -s extensions mediawiki/extensions
+rm -r mediawiki/extensions
+ln -s extensions mediawiki/extensions
+
+# run update.php
+php mediawiki/maintainance/update.php
 
 #< git submodules update >
-cp LocalSettings.php mediawiki/
+# cp LocalSettings.php mediawiki/
 
 
