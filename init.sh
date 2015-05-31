2fdddc9802e92cb14de195aa60eaeb6925295813
Toma Luca <toma.luca95@gmail.com>
2015-05-31 19:29:54 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ ! -f init.sh ]] ; then
+  echo "You are not running the init script from the WikiFM main directory. The installation script relies heavily on this. Please cd to the cloned directory and re-launch init.sh from there."
+  exit 1
+fi
+if [[ ! -f update.sh ]] ; then
+  echo "You are not running the init script from the WikiFM main directory. The installation script relies heavily on this. Please cd to the cloned directory and re-launch init.sh from there."
+  exit 1
+fi
+
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 <database name> <database user> <database user password>"
   exit 1
