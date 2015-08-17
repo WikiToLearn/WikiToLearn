843d207040a04108f322959e14faa36fee4b2c09
tomaluca95 <toma.luca95@gmail.com>
2015-08-17 13:00:05 +0200

    WikiFM -> WikiToLearn backup-mysql.sh

--- Dockers/backup-mysql.sh
+++ Dockers/backup-mysql.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 {
-cat configs/my.cnf | docker exec -i wikifm-mysql tee /root/.my.cnf
+cat configs/my.cnf | docker exec -i wikitolearn-mysql tee /root/.my.cnf
 } > /dev/null
 
 BACKUP_DIR="backups/"$(date +'%Y_%m_%d__%H_%M_%S')
 
 test -d $BACKUP_DIR || mkdir $BACKUP_DIR
 
-for db in $(docker exec -ti wikifm-mysql mysql -e "SHOW DATABASES" | grep wikifm | awk '{ print $2 }') ; do
+for db in $(docker exec -ti wikitolearn-mysql mysql -e "SHOW DATABASES" | grep wikitolearn | awk '{ print $2 }') ; do
  BACKUP_FILE=$BACKUP_DIR"/"$db
  BACKUP_FILE_STRUCT=$BACKUP_FILE".struct.sql"
  BACKUP_FILE_DATA=$BACKUP_FILE".data.sql"
 
- docker exec -ti wikifm-mysql mysqldump --skip-add-drop-table --skip-comments -d $db > $BACKUP_FILE_STRUCT
+ docker exec -ti wikitolearn-mysql mysqldump --skip-add-drop-table --skip-comments -d $db > $BACKUP_FILE_STRUCT
  sed -i 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' $BACKUP_FILE_STRUCT
 
- docker exec -ti wikifm-mysql mysqldump --no-create-info $db > $BACKUP_FILE_DATA
+ docker exec -ti wikitolearn-mysql mysqldump --no-create-info $db > $BACKUP_FILE_DATA
 done
