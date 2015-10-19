45264aa9898a1246f7eca359070087541aa8db38
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2015-10-19 11:26:01 +0200

    Fixed import and fetch dump-xml scripts

--- utils/import-xml-dump.sh
+++ utils/import-xml-dump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec w2l-dev-websrv sh -c "/var/www/WikiToLearn/lang-foreach.sh importDump.php /var/www/WikiToLearn/utils/developer-dump.xml"
+docker exec w2l-dev-websrv sh -c "/var/www/WikiToLearn/lang-foreach.sh importDump.php /var/www/WikiToLearn/developer-dump.xml"
