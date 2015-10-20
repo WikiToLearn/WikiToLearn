c458a221073ce74d8712afa6f7a20c0630ceb030
Davide Valsecchi <valsecchi.davide94@gmail.com>
2015-10-21 01:34:33 +0200

    Changed ocg-dev building

---
+++ Dockers/ocg-dev/install_ocg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /var/lib/ocg/mw-ocg-service
+npm install
+cd /var/lib/ocg/mw-ocg-bundler
+npm install
+cd /var/lib/ocg/mw-ocg-latexer
+npm install
+cd /var/lib/ocg/mw-ocg-texter
+npm install
