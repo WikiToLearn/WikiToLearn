7f7de78d24b5b305a6b74b91a6a273b155a4dd34
Luca Toma <toma.luca95@gmail.com>
2015-11-23 21:58:49 +0100

    Multiple domain now recognize domain itself

--- domains.php
+++ domains.php
@@ -14,3 +14,6 @@ $wiki_hostname = strtolower(isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NA
 $wiki = substr($wiki_hostname, 0, strpos($wiki_hostname, "."));
 $wiki_domain = substr($wiki_hostname, strlen($wiki) + 1);
 
+if(in_array($wiki_hostname,$wiki_allow_domains)) {
+ $wiki_domain = $wiki_hostname;
+}
