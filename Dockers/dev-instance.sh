969d78373fb7f9b6b56ad784db9588255ff5562c
Toma Luca <toma.luca95@gmail.com>
2015-12-13 13:21:08 +0100

    enforce developer to use develop or hotfix branch

--- Dockers/dev-instance.sh
+++ Dockers/dev-instance.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 cd "$(dirname "$(readlink "$0" || printf %s "$0")")"
+
+which git &> /dev/null
+if [[ $? -ne 0 ]] ; then
+ echo "You haven't git, this is a problem. You must install it before continue"
+ exit 1
+fi
+
+curbranch=$(git name-rev --name-only HEAD)
+if [[ "$curbranch" != "develop" ]] ; then
+ if [[ "$curbranch" != "hotfix" ]] ; then
+  echo "You aren't in 'develop' branch, this is a problem. You must 'git checkout develop' or 'git checkout hotfix'"
+  exit 1
+ fi
+fi
+echo "Ok, starting..."
+
 export W2L_PRODUCTION=0
 ./run.sh
 ./use-instance.sh
