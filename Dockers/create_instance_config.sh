845088db2dfc836433916960c18b4bc6c14590eb
Riccardo Iaconelli <riccardo@kde.org>
2016-01-11 12:06:11 +0100

    Update reference
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- Dockers/create_instance_config.sh
+++ Dockers/create_instance_config.sh
@@ -23,7 +23,7 @@ if [[ ! -f instance_config.conf ]] ; then
   echo "export W2L_DOCKER_MYSQL=mysql:5.6"
   echo "export W2L_DOCKER_MEMCACHED=memcached:1.4.24"
   echo "export W2L_DOCKER_OCG=wikitolearn/ocg:0.7.1"
-  echo "export W2L_DOCKER_WEBSRV=wikitolearn/websrv:0.10.1"
+  echo "export W2L_DOCKER_WEBSRV=wikitolearn/websrv:0.11"
   echo "export W2L_DOCKER_HAPROXY=wikitolearn/haproxy:0.5"
  } > instance_config.conf
  echo
