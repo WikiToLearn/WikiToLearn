5bf0caf0e4dbc38fc72ec945bbfae7522a56c9ab
Riccardo Iaconelli <riccardo@kde.org>
2016-01-11 12:50:50 +0100

    add new domains
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- domains.php
+++ domains.php
@@ -1,10 +1,12 @@
 <?php
 $wiki_allow_domains = array(
     "wikitolearn.org",
+    "wikitolearn.vodka",
     "dev.wikitolearn.org",
     "pre.wikitolearn.org",
     "local.wikitolearn.org",
     "direct.wikitolearn.org",
+    "tuttorotto.it",
     "tuttorotto.eu",
     "tuttorotto.org",
     "tuttorotto.biz"
