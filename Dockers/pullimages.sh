7c449e7b16b4ef15bcc7f003bb5d86557c1525e2
Toma Luca <toma.luca95@gmail.com>
2016-03-04 11:31:11 +0100

    BUGFIX for allow OCG skip also in the pullimages script

--- Dockers/pullimages.sh
+++ Dockers/pullimages.sh
@@ -9,11 +9,17 @@ if [[ ! -f instance_config.conf ]] ; then
 fi
 . ./instance_config.conf
 
-if [[ "$W2L_SKIP_OCG_DOCKER" == "0" ]] ; then
-  W2L_DOCKER_OCG_USE="$W2L_DOCKER_OCG"
+W2L_DOCKER_OCG_USE="$W2L_DOCKER_OCG"
+
+if [[ "$W2L_SKIP_OCG_DOCKER" == "1" ]] ; then
+ W2L_DOCKER_OCG_USE=""
 fi
 
-
-for img in $W2L_DOCKER_MEMCACHED $W2L_DOCKER_MYSQL $$W2L_DOCKER_OCG_USE $W2L_DOCKER_WEBSRV $W2L_DOCKER_HAPROXY $W2L_DOCKER_PARSOID $W2L_DOCKER_MATHOID ; do
- echo docker pull $img
+for img in $W2L_DOCKER_MEMCACHED $W2L_DOCKER_MYSQL $W2L_DOCKER_OCG_USE $W2L_DOCKER_WEBSRV $W2L_DOCKER_HAPROXY $W2L_DOCKER_PARSOID $W2L_DOCKER_MATHOID ; do
+ docker pull $img
+ docker inspect $img &> /dev/null
+ if [[ $? -ne 0 ]] ; then
+  echo "Error downloading the "$img" image, run again the script"
+  exit 1
+ fi
 done
