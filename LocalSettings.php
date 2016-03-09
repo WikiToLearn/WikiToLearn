eb210601c978a83d968d4f66d6670b1f992fe79d
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-03-09 19:00:54 +0100

    Enable the sidebar to be editable

--- LocalSettings.php
+++ LocalSettings.php
@@ -244,7 +244,7 @@ wfLoadExtension( "ParserFunctions" );
 # Bigger uploads
 $wgMaxUploadSize = 2147483648;
 
-# Protect only uploads 
+# Protect only uploads
 $wgAllowExternalImagesFrom = array('http://www.' . $wiki_domain . '/', 'http://www.pledgie.com');
 
 $wgUseETag = true;
@@ -411,3 +411,41 @@ $wgVirtualRestConfig['modules']['parsoid'] = array(
   // Parsoid "domain", see below (optional)
   'domain' => isset($_SERVER['SERVER_NAME'])?$_SERVER['SERVER_NAME']:"",
 );
+
+// Licence WTFPL 2.0
+// Modifies the toolbar to be editable
+$wgHooks['BaseTemplateToolbox'][] = 'modifyToolbox';
+
+function modifyToolbox( BaseTemplate $baseTemplate, array &$toolbox ) {
+
+	static $keywords = array( 'WHATLINKSHERE', 'RECENTCHANGESLINKED', 'FEEDS', 'CONTRIBUTIONS', 'LOG', 'BLOCKIP', 'EMAILUSER', 'USERRIGHTS', 'UPLOAD', 'SPECIALPAGES', 'PRINT', 'PERMALINK', 'INFO' );
+
+	$modifiedToolbox = array();
+
+	// Walk in the MediaWiki:Sidebar message, section toolbox
+	foreach ( $baseTemplate->data['sidebar']['TOOLBOX'] as $value ) {
+		$specialLink = false;
+
+		// Search if the keyword exists
+		foreach ( $keywords as $key ) {
+			if ( $value['href'] == Title::newFromText($key)->fixSpecialName()->getLinkURL() ) {
+				$specialLink = true;
+
+				// This is a keyword, hence add this special link
+				if ( array_key_exists( strtolower($key), $toolbox ) ) {
+					$modifiedToolbox[strtolower($key)] = $toolbox[strtolower($key)];
+					break;
+				}
+			}
+		}
+
+		// This is a normal link
+		if ( !$specialLink ) {
+			$modifiedToolbox[] = $value;
+		}
+	}
+
+	$toolbox = $modifiedToolbox;
+
+	return true;
+}
