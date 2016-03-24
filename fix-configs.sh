9afce357ab8cb76591358f99765a501fb8b0d55b
Toma Luca <toma.luca95@gmail.com>
2016-03-24 18:01:12 +0100

    clean up and addedd some usefull file
    composer-dirs.conf: list of directory where we need the "composer install"
    fix-configs.sh:     script that must init the configs for wtl

---
+++ fix-configs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script must init all required configs and return exit status 0
+# if the exit status is not 0 we assume some critical config missing and in this case wtl can be started
