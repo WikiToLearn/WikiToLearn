ecb77fc5053dcaff97a240313d6fed53bf312df8
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-04-01 22:52:14 +0200

    Remove the editable sidebar hack

--- LocalSettings.php
+++ LocalSettings.php
@@ -349,49 +349,6 @@ if (file_exists("$IP/../LocalSettings.d/wgReadOnly.php")) {
     require_once("$IP/../LocalSettings.d/wgReadOnly.php");
 }
 
-
-
-
-
-// Licence WTFPL 2.0
-// Modifies the toolbar to be editable
-$wgHooks['BaseTemplateToolbox'][] = 'modifyToolbox';
-
-
-function modifyToolbox( BaseTemplate $baseTemplate, array &$toolbox ) {
-
-    static $keywords = array( 'WHATLINKSHERE', 'RECENTCHANGESLINKED', 'FEEDS', 'CONTRIBUTIONS', 'LOG', 'BLOCKIP', 'EMAILUSER', 'USERRIGHTS', 'UPLOAD', 'SPECIALPAGES', 'PRINT', 'PERMALINK', 'INFO' );
-
-    $modifiedToolbox = array();
-
-    // Walk in the MediaWiki:Sidebar message, section toolbox
-    foreach ( $baseTemplate->data['sidebar']['TOOLBOX'] as $value ) {
-        $specialLink = false;
-
-        // Search if the keyword exists
-        foreach ( $keywords as $key ) {
-            if ( $value['href'] == Title::newFromText($key)->fixSpecialName()->getLinkURL() ) {
-                $specialLink = true;
-
-                // This is a keyword, hence add this special link
-                if ( array_key_exists( strtolower($key), $toolbox ) ) {
-                    $modifiedToolbox[strtolower($key)] = $toolbox[strtolower($key)];
-                    break;
-                }
-            }
-        }
-
-        // This is a normal link
-        if ( !$specialLink ) {
-            $modifiedToolbox[] = $value;
-        }
-    }
-
-    $toolbox = $modifiedToolbox;
-
-    return true;
-}
-
 // SubapageList needs it
 require_once( "$IP/extensions/ParserHooks/ParserHooks.php" );
 #require_once( "$IP/extensions/SubPageList/SubPageList.php" );
@@ -425,4 +382,3 @@ wfLoadExtension( "WikiEditor" );
 $wgDefaultUserOptions['usebetatoolbar'] = 1;
 $wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
 $wgDefaultUserOptions['wikieditor-preview'] = 1;
-
