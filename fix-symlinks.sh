85657226841e9049034067c807a33c598343dad2
Toma Luca <toma.luca95@gmail.com>
2016-09-27 14:50:15 +0200

    moved skins to proper directory
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- fix-symlinks.sh
+++ fix-symlinks.sh
@@ -9,16 +9,16 @@ CWD="$(
 
 #remove the existing symlinks
 test -e $CWD/mediawiki/LocalSettings.php && rm -f $CWD/mediawiki/LocalSettings.php
-test -e $CWD/mediawiki/skins/Neverland && rm -f $CWD/mediawiki/skins/Neverland
 test -e $CWD/mediawiki/favicon.ico && rm -f $CWD/mediawiki/favicon.ico
+rm -fr $CWD/mediawiki/skins
 rm -fr $CWD/mediawiki/extensions
 
 
 #create symlinks
-ln -s ../LocalSettings.php mediawiki/LocalSettings.php
-ln -s ../../Neverland mediawiki/skins/Neverland
-ln -s ../extensions mediawiki/extensions
-ln -s ../favicon.ico mediawiki/favicon.ico
-
+cd $CWD/mediawiki/
+ln -s ../LocalSettings.php LocalSettings.php
+ln -s ../extensions        extensions
+ln -s ../skins             skins
+ln -s ../favicon.ico       favicon.ico
 
 
