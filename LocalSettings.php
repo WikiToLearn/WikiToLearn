8c9c824ee38dd27071068a71a19a21bff2db0fc4
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-09-29 13:12:42 +0200

    Bump CourseEditor, Bump WikiToLearnSkin, removed debug toolbar, added en as supportedLanguage
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- LocalSettings.php
+++ LocalSettings.php
@@ -6,7 +6,6 @@
 #
 # Further documentation for configuration settings may be found at:
 # http://www.mediawiki.org/wiki/Manual:Configuration_settings
-$wgDebugToolbar = true;
 if (getenv("WTL_PRODUCTION") != "1"){
   error_reporting(-1);
   ini_set("display_errors",1);
@@ -101,7 +100,7 @@ $wgSharedTables[] = array('user', 'user_properties', 'user_groups', 'interwiki',
 # Make sure you give permission to sharedwikitolearn database to the user in question.
 $wgLanguageCode = "en"; // Default
 #Officially supported languages
-$wgSupportedLanguages = array('ca', 'de', 'es', 'fr', 'it');
+$wgSupportedLanguages = array('ca', 'de', 'en', 'es', 'fr', 'it');
 
 require_once("$IP/../LocalSettings.d/mysql-username-and-password.php");
 require_once("$IP/../LocalSettings.d/wgSecretKey.php");
