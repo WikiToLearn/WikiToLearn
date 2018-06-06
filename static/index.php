2f2ea62418f437ce0ead7b270093c8926ac8c548
Toma Luca <toma.luca95@gmail.com>
2018-06-06 10:25:12 +0200

    fixed typo in the static index page
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- static/index.php
+++ static/index.php
@@ -36,8 +36,8 @@ if ($domain != 'local.wikitolearn-test.org' && !$https) {
         <meta property="og:site_name" content="WikiToLearn"/>
         <meta name="twitter:card" content="summary"/>
         <meta name="twitter:site" content="@WikiToLearn"/>
-        <meta name="twitter:image" content="http://www.<?php echo $domain; ?>/wikitolearn.jpg"/>
-        <meta property="og:image" content="http://www.<?php echo $domain; ?>/wikitolearn.jpg"/>
+        <meta name="twitter:image" content="https://www.<?php echo $domain; ?>/assets/v1/img/wikitolearn.jpg"/>
+        <meta property="og:image" content="https://www.<?php echo $domain; ?>/assets/v1/img/wikitolearn.jpg"/>
         <meta property="og:image:width" content="500" />
         <meta property="og:image:height" content="500" />
         <link rel="stylesheet" href="/assets/v1/bootstrap-3.3.7-dist/css/bootstrap.min.css">
